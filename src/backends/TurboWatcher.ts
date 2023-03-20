028b1159283b9632bf66454af6049edd8ff3d415
Gajus Kuizinas <gajus@gajus.com>
2023-03-20 02:51:43 -0600

    feat: abstract backend

--- src/backends/TurboWatcher.ts
+++ src/backends/TurboWatcher.ts
@@ -15,17 +15,13 @@ const isMacOs = () => {
   return platform() === 'darwin';
 };
 
-const isLinux = () => {
-  return platform() === 'linux';
-};
-
 export class TurboWatcher extends FileWatchingBackend {
   private backend: FileWatchingBackend;
 
   public constructor(project: string) {
     super();
 
-    if (semver.gte(process.version, '19.1.0') && (isMacOs() || isLinux())) {
+    if (semver.gte(process.version, '19.1.0') && isMacOs()) {
       log.info('using native FSWatcher');
       this.backend = new FSWatcher(project);
     } else {
