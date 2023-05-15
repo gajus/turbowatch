030c90bfcec795c3e9e95e5c436e4b07f0cddf6d
Gajus Kuizinas <gajus@gajus.com>
2023-05-15 14:51:34 -0600

    fix: correct defineConfig logic

--- src/defineConfig.ts
+++ src/defineConfig.ts
@@ -1,9 +1,11 @@
+import { TurboWatcher } from './backends/TurboWatcher';
 import { type TurbowatchConfigurationInput } from './types';
 
 export const defineConfig = (
   configurationInput: TurbowatchConfigurationInput,
 ): TurbowatchConfigurationInput => {
   return {
+    Watcher: TurboWatcher,
     ...configurationInput,
   };
 };
