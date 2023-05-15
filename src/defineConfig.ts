1992ef7903462242921d42222da55d1e486ad847
Gajus Kuizinas <gajus@gajus.com>
2023-05-15 14:26:27 -0600

    docs: document abortController default value

--- src/defineConfig.ts
+++ src/defineConfig.ts
@@ -8,6 +8,8 @@ export const defineConfig = (
   configurationInput: TurbowatchConfigurationInput,
 ): TurbowatchConfiguration => {
   return {
+    // This would not be used in practice, but it's here to make the type system happy.
+    // bin/turbowatch.ts always overrides this.
     abortController: new AbortController(),
 
     // as far as I can tell, this is a bug in unicorn/no-unused-properties
