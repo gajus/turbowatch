7e45db49b269ab0c2b031f95561e2904ed110a84
Gajus Kuizinas <gajus@gajus.com>
2023-05-15 14:25:33 -0600

    fix: add fallback AbortController

--- src/defineConfig.ts
+++ src/defineConfig.ts
@@ -8,6 +8,8 @@ export const defineConfig = (
   configurationInput: TurbowatchConfigurationInput,
 ): TurbowatchConfiguration => {
   return {
+    abortController: new AbortController(),
+
     // as far as I can tell, this is a bug in unicorn/no-unused-properties
     // https://github.com/sindresorhus/eslint-plugin-unicorn/issues/2051
     // eslint-disable-next-line unicorn/no-unused-properties
