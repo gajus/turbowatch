8247ef3824a56fef4ba8ecb3d78eb0ac019298fa
Gajus Kuizinas <gajus@gajus.com>
2023-05-15 14:42:09 -0600

    fix: correct defineConfig logic

--- src/defineConfig.ts
+++ src/defineConfig.ts
@@ -1,26 +1,9 @@
-import { TurboWatcher } from './backends/TurboWatcher';
-import {
-  type TurbowatchConfiguration,
-  type TurbowatchConfigurationInput,
-} from './types';
+import { type TurbowatchConfigurationInput } from './types';
 
 export const defineConfig = (
   configurationInput: TurbowatchConfigurationInput,
-): TurbowatchConfiguration => {
+): TurbowatchConfigurationInput => {
   return {
-    // This would not be used in practice, but it's here to make the type system happy.
-    // bin/turbowatch.ts always overrides this.
-    abortController: new AbortController(),
-
-    // as far as I can tell, this is a bug in unicorn/no-unused-properties
-    // https://github.com/sindresorhus/eslint-plugin-unicorn/issues/2051
-    // eslint-disable-next-line unicorn/no-unused-properties
-    debounce: {
-      wait: 1_000,
-    },
-
-    // eslint-disable-next-line unicorn/no-unused-properties
-    Watcher: TurboWatcher,
     ...configurationInput,
   };
 };
